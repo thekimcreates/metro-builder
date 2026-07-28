21603853ea45792d8a796be01288d70eee8dc5b7
thekimcreates <joshuayulkim111014@gmail.com>
2026-07-28 15:00:01 -0700

    Refactor package structure and update initialization

--- src/main/java/dev/metrobuilder/MetroBuilder.java
+++ src/main/java/dev/metrobuilder/MetroBuilder.java
@@ -1,17 +1,18 @@
-package dev.metrobuilder;
+package com.thekimcreates.metrobuilder;
 
-import dev.metrobuilder.network.MetroBuilderNetworking;
+import com.thekimcreates.metrobuilder.item.MetroBuilderItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class MetroBuilder implements ModInitializer {
+
     public static final String MOD_ID = "metrobuilder";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        MetroBuilderNetworking.registerServerReceivers();
-        LOGGER.info("MetroBuilder {} initialized", "0.1.0-alpha");
+        MetroBuilderItems.register();
+        LOGGER.info("MetroBuilder common initialization complete");
     }
 }
